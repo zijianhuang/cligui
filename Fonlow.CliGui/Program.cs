bc56b638650b97de70b4300a38d01d14a9fd7827
zijianhuang <zijianhuang2000@gmail.com>
2023-11-22 16:42:07 +1000

    initial

---
+++ Fonlow.CliGui/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+using Fonlow.CommandLineGui.Gui;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace Fonlow.CommandLineGui
+{
+	[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			System.Reflection.Assembly appAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+			string dirOfAppAssembly = System.IO.Path.GetDirectoryName(appAssembly.Location);
+			IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile(System.IO.Path.Combine(dirOfAppAssembly, "appsettings.json")).Build();
+			var appSettings = config.GetSection("cliSettings");
+			var cliSettings = appSettings.Get<Settings>();
+			
+			switch (cliSettings.PluginAllocationMethod)
+			{
+				case PluginAllocationMethod.Mono:
+					string assemblyName = cliSettings.AssemblyNameOfCommand;//Better to use the first in the list rather than AssemblyNameOfCommand which is in UserScope.
+					if (String.IsNullOrEmpty(assemblyName))
+						assemblyName = "RobocopyParametersCore";
+					var command = CommandFactory.CreateCommandFromAssembly(assemblyName);
+					if (command != null)
+					{
+						using (MainForm form = new MainForm(command, cliSettings))
+						{
+							form.Left = 0;
+							form.Top = 0;
+							form.Height = Screen.PrimaryScreen.WorkingArea.Height;
+
+							Application.Run(form);
+						}
+					}
+					else
+					{
+						System.Diagnostics.Trace.TraceError("fail");
+						MessageBox.Show(String.Format("Cannot load {0} to initialize.", assemblyName));
+					}
+
+					break;
+				case PluginAllocationMethod.Registration:
+					RunMainFormWithMultipleCommands(cliSettings);
+					break;
+				default:
+					throw new InvalidOperationException("Is this possible?");
+			}
+		}
+
+		static void RunMainFormWithMultipleCommands(Settings settings)
+		{
+			using (MainForm form = new MainForm(settings.AssemblyNames, settings.AssemblyNameOfCommand))
+			{
+				form.Left = 0;
+				form.Top = 0;
+				form.Height = Screen.PrimaryScreen.WorkingArea.Height;
+
+				Application.Run(form);
+			}
+
+		}
+	}
+}
---
+++ RobocopyParametersCore/UserControl1.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace Fonlow.CommandLineGui.Robocopy
+{
+	partial class UserControl1
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			SuspendLayout();
+			// 
+			// UserControl1
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			Name = "UserControl1";
+			Size = new System.Drawing.Size(522, 138);
+			ResumeLayout(false);
+		}
+
+		#endregion
+	}
+}
